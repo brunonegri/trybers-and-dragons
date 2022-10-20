a9bc7e9dde1d7753a8374b380aaf32ec45384606
Bruno Negri <b.negri_@hotmail.com>
2022-10-20 10:46:54 -0300

    Req 2

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,10 @@
 import Race from './Race';
+import Dwarf from './Dwarf';
+import Elf from './Elf';
+import Halfling from './Halfling';
+import Orc from './Orc';
+
+// const test = new Dwarf('DUrin', 40);
 
 export default Race;
+export { Dwarf, Elf, Halfling, Orc };
